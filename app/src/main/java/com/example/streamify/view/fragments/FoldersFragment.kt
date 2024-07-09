d2b4655ca064aea26e1747d122b3fc62b09ff826
Supriya Kashyap <kashyapsupriya61275@gmail.com>
2024-07-10 00:10:33 +0530

    some more changes

--- app/src/main/java/com/example/streamify/view/fragments/FoldersFragment.kt
+++ app/src/main/java/com/example/streamify/view/fragments/FoldersFragment.kt
@@ -1,20 +1,21 @@
 package com.example.streamify.view.fragments
 
 import android.annotation.SuppressLint
+import android.graphics.Rect
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.streamify.MainActivity
 import com.example.streamify.R
 import com.example.streamify.databinding.FragmentFoldersBinding
 import com.example.streamify.view.adapters.FoldersAdapter
 
-
 class FoldersFragment : Fragment() {
-
     private lateinit var binding: FragmentFoldersBinding
 
     @SuppressLint("SetTextI18n")
@@ -34,12 +35,25 @@ class FoldersFragment : Fragment() {
     }
 
     private fun setupRecyclerView() {
+        val gridLayoutManager = GridLayoutManager(requireContext(), 2)
+
         binding.FoldersRV.apply {
             setHasFixedSize(true)
             setItemViewCacheSize(10)
-            layoutManager = LinearLayoutManager(requireContext())
+            layoutManager = gridLayoutManager
             adapter = FoldersAdapter(requireContext(), MainActivity.folderList)
+            addItemDecoration(SpacingItemDecoration(10))
         }
         binding.totalFolders.text = "Total Folders: ${MainActivity.folderList.size}"
+    }
+    class SpacingItemDecoration(private val space: Int) : RecyclerView.ItemDecoration() {
+        override fun getItemOffsets(outRect: Rect, view: View, parent: RecyclerView, state: RecyclerView.State) {
+            outRect.left = space
+            outRect.right = space
+            outRect.bottom = space
+            if (parent.getChildLayoutPosition(view) == 0 || parent.getChildLayoutPosition(view) == 1) {
+                outRect.top = space
+            }
+        }
     }
 }
